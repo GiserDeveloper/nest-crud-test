438a3ccc5536314df1c9019e9e9307b265afd437
张童瑶 <864443272@qq.com>
2020-08-04 15:52:13 +0800

    完成：CRUD测试

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('快速CRUD测试-mongodb')
+    .setDescription('使用NestJS构建的服务端API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
+  console.log('http://localhost:3000/api-docs')
 }
 bootstrap();
